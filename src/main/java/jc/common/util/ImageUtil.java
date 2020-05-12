34410c0cfbc3bfae333b80e536a913c999671c21
zerochen2016 <470133773@qq.com>
2020-05-12 16:38:39 +0800

    watermark

--- src/main/java/jc/common/util/ImageUtil.java
+++ src/main/java/jc/common/util/ImageUtil.java
@@ -3,8 +3,15 @@ package jc.common.util;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 /**
  * util of image
@@ -85,5 +92,88 @@ public class ImageUtil {
                 ran.nextInt(256), ran.nextInt(256));  
         return color;  
     }  
+    
+    /**
+     * 添加水印
+     * @param srcImgPath 图片路径
+     * @param watermarkContent 水印内容
+     * @param color 颜色
+     * @param font 字体
+     */
+	public static void addWatermark(String srcImgPath, String watermarkContent,Color color,Font font) {
+		
+		try {
+			//获取图片文件
+			File srcImgfile = new File(srcImgPath);
+			//把文件转换成图片
+			Image srcImg = ImageIO.read(srcImgfile);
+			//获取图片的宽和高
+			int srcImgwidth = srcImg.getWidth(null);
+			int srcImgheight = srcImg.getHeight(null);
+			
+			//画水印需要一个画板    创建一个画板
+			BufferedImage buffImg = new BufferedImage(srcImgwidth,srcImgheight,BufferedImage.TYPE_INT_RGB);
+			//创建一个2D的图像
+			Graphics2D g = buffImg.createGraphics();
+			//画出来
+			g.drawImage(srcImg, 0, 0, srcImgwidth, srcImgheight,null);
+			
+			//设置水印的颜色
+			g.setColor(color);
+			
+			//设置水印的字体
+			g.setFont(font);
+			
+			//设置水印坐标
+			int x = srcImgwidth * 19/20 - getwatermarkLength(watermarkContent, g);
+			int y = srcImgheight * 9/10;
+			
+			//根据获取的坐标 在相应的位置画出水印
+			g.drawString(watermarkContent, x, y);
+			
+			//释放画板的资源
+			g.dispose();
+			
+			//输出新的图片
+			FileOutputStream outputStream = new FileOutputStream(srcImgfile);
+			
+			//创建新的图片
+			ImageIO.write(buffImg, "jpg", outputStream);
+			
+			
+			//刷新流
+			outputStream.flush();
+			//关闭流
+			outputStream.close();
+			
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 获取水印坐标
+	 * @param watermarkContent 水印内容
+	 * @param g
+	 * @return
+	 */
+	public static int getwatermarkLength(String watermarkContent,Graphics2D g) {
+		return	g.getFontMetrics(g.getFont()).charsWidth(watermarkContent.toCharArray(), 0, watermarkContent.length());
+				
+	}
+	
+	public static void main(String[] args) {
+		
+		Font font = new Font("微软雅黑",Font.PLAIN,20);
+		
+		String srcImgPath = "图片路径";
+		
+		String watermarkContent = "水印";
+		Color color = new Color(255, 255, 255,128);
+		
+		ImageUtil.addWatermark(srcImgPath, watermarkContent, color, font);
+		
+	}
     
 }
